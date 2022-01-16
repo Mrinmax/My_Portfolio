38db427cee1b4c1d5599f39a1f4d2365bbe81fa8
Mrinal kanti sardar <64627885+Mrinmax@users.noreply.github.com>
2022-01-17 02:21:41 +0530

    close to

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components';
+import {DarkTheme} from './Themes';
+
+import LogoComponent from '../subComponents/LogoComponent';
+import Social from '../subComponents/Social';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+
+import astronaut from '../assets/Images/spaceman.png';
 
 function About() {
     return (
-        <div>
-            About Page
-        </div>
+        <ThemeProvider theme={DarkTheme}>
+            <Box>
+                <LogoComponent theme='dark'/>
+                <Social theme='dark'/>
+                <PowerButton />
+                <ParticleComponent theme='dark' />
+
+                <Spaceman>
+                    <img src={astronaut} alt="spaceman" />
+                </Spaceman>
+               
+            </Box>
+        </ThemeProvider>
     )
 }
 
 export default About;
+
+const Box = styled.div`
+    background-color: ${props => props.theme.body};
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+
+`
+
+const Spaceman = styled.div`
+    position: absolute;
+    top: 10%;
+    right: 5%;
+    width: 20vw;
+
+    img{
+        width: 100%;
+        height: auto;
+    }
+
+
+`
+
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,14 +3,23 @@ import styled, { ThemeProvider } from 'styled-components';
 import {lightTheme} from './Themes';
 import {Design, Develope} from './AllSvgs';
 
+import LogoComponent from '../subComponents/LogoComponent';
+import Social from '../subComponents/Social';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+
 
 function Skill() {
     return (
         <ThemeProvider theme={lightTheme}>
             <Box>
+                <LogoComponent theme='light'/>
+                <Social theme='light'/>
+                <PowerButton />
+                <ParticleComponent theme='light' />
                 <Main>
                     <Title>
-                        <Design width={40} height={40} />Designer
+                        <Design width={40} height={40} /> Designer
                     </Title>
                     <Description>
                         I love to create smooth UIs and make myself happy to build awesome websites.
@@ -22,10 +31,16 @@ function Skill() {
                             <li>Mobile Apps</li>
                         </ul>
                     </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <ul>
+                            <li>Figma</li>
+                        </ul>
+                    </Description>
                 </Main>
                 <Main>
                     <Title>
-                        <Develope width={40} height={40} />Frontend Developer
+                        <Develope width={40} height={40} /> Frontend Developer
                     </Title>
                     <Description>
                         There are three responses to a piece of design — yes, no, and WOW! Wow is the one to aim for!
@@ -33,6 +48,11 @@ function Skill() {
                     <Description>
                         <strong>Skills</strong>
                         <p>Html, Css, Js, Ts, React, Next, Redux, TailwindCss, Firebase etc.</p>
+                    </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <p>Vscode, Github, codepen etc.</p>
+                        
                     </Description>
                 </Main>
             </Box>
@@ -68,13 +88,28 @@ const Main = styled.div`
     flex-direction: column;
     justify-content: space-between;
 
+    &:hover {
+        color: ${props => props.theme.body};
+        background-color: ${props => props.theme.text};
+    }
+
 `
 
-const Title = styled.h2`
+const Title = styled.h1`
     display: flex;
     justify-content: center;
     align-items: center;
     font-sixe: calc(1em + 1vw);
+
+    ${Main}:hover &{
+        &>*{
+            fill:${props => props.theme.body};
+        }
+    }
+
+    &>*:first-child{
+        margin-right: 1rem;
+    }
 
 `
 
@@ -82,5 +117,18 @@ const Description = styled.div`
     color: ${props => props.theme.text};
     font-size: calc(0.6em + 1vw);
     padding: 0.5rem 0;
+
+    ${Main}:hover &{
+        color: ${props => props.theme.body};
+    }
+
+    strong{
+        margin-bottom: 1rem;
+        text-transform: uppercase;
+    }
+
+    ul,p{
+        margin-left: 2rem;
+    }
 
 `
